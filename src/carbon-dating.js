5815445094fb7a0ae6e50d3c58a26d439374feea
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-26 02:02:56 +0200

    feat: add carbon-dating method

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+	if (typeof sampleActivity !== 'string' || sampleActivity === undefined || !Boolean(Number(sampleActivity)) || Number(sampleActivity) < 0 ) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / k);
+  return t > 0 ? t : false;
 }
 
 module.exports = {
