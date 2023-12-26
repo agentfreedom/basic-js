fab79b921d91fcbbb65eda69d449be5b7ce222fa
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-26 02:11:33 +0200

    feat: add delete-digdt method

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+	let number = String(n).slice(1);
+  for (i = 1; i < String(n).length; i += 1) {
+    let x = String(n).substring(0, i) + String(n).substring(i + 1)
+    if (Number(x) > Number(number)) {
+      number = x;
+    }
+  }
+  return Number(number);
 }
 
 module.exports = {
