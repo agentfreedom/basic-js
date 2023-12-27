d7b2baa533068b984cb60bd6d8b97ae3a8769099
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-28 00:21:14 +0200

    feat: add extended-repeater method

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+	const {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  } = options;
+
+  const additionString = Array(additionRepeatTimes)
+    .fill(String(addition))
+    .join(additionSeparator);
+
+  const mainString = String(str) + additionString;
+
+  return Array(repeatTimes).fill(mainString).join(separator);
 }
 
 module.exports = {
