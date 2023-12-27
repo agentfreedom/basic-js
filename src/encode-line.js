8a16146d4507e05b5127d592513186f415575ed9
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-12-28 00:16:17 +0200

    feat: add encode-line method

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+	let res = "";
+  let count = 1;
+
+  for (let i = 1; i <= str.length; i++) {
+    if (i < str.length && str[i] === str[i - 1]) {
+      count++;
+    } else {
+      res += count === 1 ? str[i - 1] : `${count}${str[i - 1]}`;
+      count = 1;
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
